e7b56833bbb68f0afb0098a39153d83e243571a9
zhongrui3 <zhongrui3@staff.weibo.com>
2020-06-01 17:35:10 +0800

    init

--- src/main/java/txgg/data/process/FeatureProcess.scala
+++ src/main/java/txgg/data/process/FeatureProcess.scala
@@ -1,5 +1,26 @@
 package txgg.data.process
 
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
 object FeatureProcess {
+	def main(args: Array[String]): Unit = {
+		if (args.length == 0) print("Please set parameter")
+		println("args=", args.mkString(", "))
+		//        val params = SparkUtil.parse(args)
+		//        val dt = params.getOrElse("dt", "20200227")
+		val dt = args(0)
+		val predt = args(1)
+		val target_path = args(2)
+		
+		val sparkSession = SparkSession.builder().appName("TxggDataTest")
+			.getOrCreate()
+		val sparkContext = sparkSession.sparkContext
+		val sparkConf = sparkContext.getConf
+		val sqlContext = new SQLContext(sparkContext)
+		val numPartitions = 460
+		val dataPath = "hdfs://ns3-backup/user/weibo_bigdata_push/zhongrui3/txgg/"
+		println("dataPath=", dataPath)
+	}
+
 	
 }
