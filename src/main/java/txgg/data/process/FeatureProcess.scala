c4c8117b69fb5f7c511f5b641eba0c4ee24abe53
zhongrui3 <zhongrui3@staff.weibo.com>
2020-06-01 17:55:20 +0800

    Fx

--- src/main/java/txgg/data/process/FeatureProcess.scala
+++ src/main/java/txgg/data/process/FeatureProcess.scala
@@ -1,16 +1,13 @@
 package txgg.data.process
 
 import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.storage.StorageLevel
 
 object FeatureProcess {
 	def main(args: Array[String]): Unit = {
 		if (args.length == 0) print("Please set parameter")
 		println("args=", args.mkString(", "))
-		//        val params = SparkUtil.parse(args)
-		//        val dt = params.getOrElse("dt", "20200227")
-		val dt = args(0)
-		val predt = args(1)
-		val target_path = args(2)
+		val func_name = args(0)
 		
 		val sparkSession = SparkSession.builder().appName("TxggDataTest")
 			.getOrCreate()
@@ -18,9 +15,31 @@ object FeatureProcess {
 		val sparkConf = sparkContext.getConf
 		val sqlContext = new SQLContext(sparkContext)
 		val numPartitions = 460
-		val dataPath = "hdfs://ns3-backup/user/weibo_bigdata_push/zhongrui3/txgg/"
+		val dataPath = "/home/fzr/txgg/data/origin/"
 		println("dataPath=", dataPath)
+		println("funcname=", func_name)
+		val train_path = dataPath+"/train_preliminary/"
+		val test_path = dataPath+"/test/"
+		val train_ad_data = sparkSession.sparkContext.textFile(train_path + "/ad.csv")
+			.map(p => p.split(","))
+			.map(p => (p(0), p(1), p(2), p(3), p(4), p(5))).repartition(numPartitions)
+			.persist(StorageLevel.MEMORY_AND_DISK)
+		train_ad_data.take(10).foreach(println)
+//		val test_ad_data = sparkSession.sparkContext.textFile(test_path+"/ad.csv")
+//			.map(p => p.split(","))
+//			.map(p => (p(0), p(1), p(2), p(3), p(4), p(5))).repartition(numPartitions)
+//			.persist(StorageLevel.MEMORY_AND_DISK)
+		if (func_name == "userfeature") {
+//			userFeatureProcess(sparkSession, dataPath, numPartitions)
+		} else if(func_name == "adfeature"){
+//			adFeatureProcess(sparkSession, dataPath, numPartitions)
+		}
+		else {
+			println("please enter a funcname: userfeature / sequence_uid_ad / makegraph / ad_list")
+		}
+		
 	}
+	
 
 	
 }
