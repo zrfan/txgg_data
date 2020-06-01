fd5c06d25454e52599c61b9a4af89c09ef8983e5
zrfan <fanzhongruis@163.com>
2020-06-01 23:16:51 +0800

    Fx

--- src/main/java/txgg/data/process/FeatureProcess.scala
+++ src/main/java/txgg/data/process/FeatureProcess.scala
@@ -25,7 +25,7 @@ object FeatureProcess {
 		println("funcname=", func_name)
 		val all_ad_data = readAllAdData(sparkSession, dataPath, savePath, numPartitions).persist(StorageLevel.MEMORY_AND_DISK)
 		
-		all_ad_data.take(10).foreach(println)  // 去重后广告数3412773
+		all_ad_data.take(10).foreach(println) // 去重后广告数3412773
 		println("all_ad_data count=", all_ad_data.count())
 		
 		val all_click_data = readAllClickData(sparkSession, dataPath, savePath, numPartitions).persist(StorageLevel.MEMORY_AND_DISK)
@@ -38,12 +38,13 @@ object FeatureProcess {
 		val user_feature = userFeatureProcess(full_click_data, sparkSession, savePath, numPartitions)
 		
 		// 广告特征提取
+		val ad_train_feature = adTrainFeatureProcess(full_click_data, sparkSession, dataPath, numPartitions)
 		
 		
 		if (func_name == "new_user_list") {
-//			userFeatureProcess(sparkSession, dataPath, numPartitions)
-		} else if(func_name == "adfeature"){
-//			adFeatureProcess(sparkSession, dataPath, numPartitions)
+			//			userFeatureProcess(sparkSession, dataPath, numPartitions)
+		} else if (func_name == "adfeature") {
+			//			adFeatureProcess(sparkSession, dataPath, numPartitions)
 		}
 		else {
 			println("please enter a funcname: userfeature / sequence_uid_ad / makegraph / ad_list")
@@ -51,7 +52,7 @@ object FeatureProcess {
 		
 	}
 	
-	def userFeatureProcess(full_click_data: Dataset[Row], sparkSession: SparkSession, savePath: String, numPartitions: Int): Dataset[Row]={
+	def userFeatureProcess(full_click_data: Dataset[Row], sparkSession: SparkSession, savePath: String, numPartitions: Int): Dataset[Row] = {
 		val user_grouped = full_click_data.groupBy("user_id")
 		// 全部用户的平均点击数：35.679
 		val user_info = user_grouped.agg(sum("click_times").as("all_click_cnt"),
@@ -66,47 +67,52 @@ object FeatureProcess {
 		user_info.show(false)
 		user_info
 	}
-	def readAllClickData(sparkSession: SparkSession, dataPath: String, savePath: String, numPartitions: Int): Dataset[Row]={
+	
+	def readAllClickData(sparkSession: SparkSession, dataPath: String, savePath: String, numPartitions: Int): Dataset[Row] = {
 		val train_click_data = sparkSession.read.format("csv").option("header", "true")
 			.load(dataPath + "/train_preliminary/click_log.csv")
 		val test_click_data = sparkSession.read.format("csv").option("header", "true")
 			.load(dataPath + "/test/click_log.csv").repartition(numPartitions)
 		val all_click_data = train_click_data.union(test_click_data).repartition(numPartitions)
-		println("all click data")
-		all_click_data.show(false)
-		println("all click count=", all_click_data.count())
-		all_click_data
-	}
-	def readAllAdData(sparkSession: SparkSession, dataPath: String, savePath: String, numPartitions: Int): sql.DataFrame ={
-		val train_ad_data = sparkSession.sparkContext.textFile(dataPath + "/train_preliminary/ad.csv")
-			.map(p => p.split(","))
-			.map(p => (p(0), p(1), p(2), p(3), p(4), p(5)))
+		val user_data = sparkSession.read.format("csv").option("header", "true")
+			.load(dataPath + "/train_preliminary/user.csv").repartition(numPartitions)
+		val click_user_data = all_click_data.join(user_data, usingColumn = "user_id").na.fill(Map(
+			"ad_id" -> 4000000,
+			"product_id" -> 60000,
+			"product_category" -> 30,
+			"advertiser_id" -> 63000,
+			"industry" -> 400,
+			"age" -> 0,
+			"gender" -> 0))
 		
-		val test_ad_data = sparkSession.sparkContext.textFile(dataPath + "/test/ad.csv")
-			.map(p => p.split(","))
-			.map(p => (p(0), p(1), p(2), p(3), p(4), p(5)))
+		println("all click data")
+		click_user_data.show(false)
+		println("all click count=", click_user_data.count())
+		click_user_data
+	}
+	
+	def readAllAdData(sparkSession: SparkSession, dataPath: String, savePath: String, numPartitions: Int): sql.DataFrame = {
+		val train_ad_data = sparkSession.read.format("csv").option("header", "true")
+			.load(dataPath + "/train_preliminary/ad.csv")
+		
+		val test_ad_data = sparkSession.read.format("csv").option("header", "true").load(dataPath + "/test/ad.csv")
+		
 		val all_ad_data = train_ad_data.union(test_ad_data).repartition(numPartitions)
-			.map(p => (p._1,
-				if (p._2 == "\\N") "4000000" else p._2,
-				if (p._3 == "\\N") "60000" else p._3,
-				if (p._4 == "\\N") "30" else p._4,
-				if (p._5 == "\\N") "63000" else p._5,
-				if (p._6 == "\\N") "400" else p._6)).filter(p => p._1 != "creative_id")
-			.distinct()
-			.map(p => Row(p._1, p._2, p._3, p._4, p._5, p._6)) // creative_id, ad_id, product_id, product_category, advertiser_id, industry
+			.distinct() // creative_id, ad_id, product_id, product_category, advertiser_id, industry
 		val schema = StructType(List(
 			StructField("creative_id", StringType), StructField("ad_id", StringType), StructField("product_id", StringType),
 			StructField("product_category", StringType), StructField("advertiser_id", StringType), StructField("industry", StringType)
 		))
-		val ad_df = sparkSession.createDataFrame(all_ad_data, schema)
-		println("all ad count=", ad_df.count())  // 3412773
+		
+		println("all ad count=", all_ad_data.count()) // 3412773
 		println("all Ad data")
-		ad_df.show(false)
+		all_ad_data.show(false)
 		//		ad_df.repartition(2).write.format("tfrecords").option("recordType", "Example")
 		//			.mode("overwrite").save(savePath + s"/txpredict.tfrecords")
-		ad_df
+		all_ad_data
 	}
-	def adTrainFeatureProcess(train_ad_data: Dataset[Row], sparkSession: SparkSession, dataPath: String, numPartitions: Int): Unit={
+	
+	def adTrainFeatureProcess(train_ad_data: Dataset[Row], sparkSession: SparkSession, dataPath: String, numPartitions: Int): Unit = {
 		
 		println("origin_data describe")
 		train_ad_data.describe().show(false)
@@ -121,11 +127,11 @@ object FeatureProcess {
 		val target_features = Array("age")
 		val target_val = Array("click_times")
 		
-		for (k <- Array.range(0, K)){
+		for (k <- Array.range(0, K)) {
 			splits(k) = splits(k).withColumn("fold", lit(k))
 			println("split=", k)
 			//			splits(k).show(false)
-			if (k>0){
+			if (k > 0) {
 				splits(0) = splits(0).union(splits(k))
 			}
 		}
@@ -133,17 +139,17 @@ object FeatureProcess {
 		train_ad_data.unpersist()
 		var encoded_data: Dataset[Row] = null
 		// 五折统计目标编码
-		for (k <- Array.range(0, K)){
-			var k_fold = fold_data.filter("fold="+k)
-			println("start process ", k," fold data")
+		for (k <- Array.range(0, K)) {
+			var k_fold = fold_data.filter("fold=" + k)
+			println("start process ", k, " fold data")
 			k_fold.show(false)
-			val other = fold_data.filter("fold!="+k)
+			val other = fold_data.filter("fold!=" + k)
 			val encoder = targetEncoder(other, k_fold, feature_names, target_features, target_val, k)
 			println("after targetEncoder, res=")
 			encoder.show(20, false)
-			if (k>0){
+			if (k > 0) {
 				encoded_data.union(encoder)
-			}else encoded_data = encoder
+			} else encoded_data = encoder
 		}
 		println("encoded_data count=", encoded_data.count())
 		encoded_data.show(20, false)
@@ -152,36 +158,36 @@ object FeatureProcess {
 	
 	// 目标编码
 	def targetEncoder(other: Dataset[Row], k_fold: Dataset[Row], feature_names: Array[String],
-	                  target_features: Array[String], target_vals: Array[String], fold_num:Int):Dataset[Row]={
+	                  target_features: Array[String], target_vals: Array[String], fold_num: Int): Dataset[Row] = {
 		var res = k_fold
 		// compute target mean
 		// 构造特征：被各age点击的概率，各age的人均点击次数，各age的人均每天点击次数，各age人每天最多点击次数，各age人每天最少点击次数
-		for (feature_name <- feature_names){
-			for (target_feature <- target_features){
-				for(target_val <- target_vals){
+		for (feature_name <- feature_names) {
+			for (target_feature <- target_features) {
+				for (target_val <- target_vals) {
 					val tmp = other.select(feature_name, target_feature, target_val, "user_id", "time").persist(StorageLevel.MEMORY_AND_DISK)
 					println("feature_name=", feature_name, "    target=", target_feature, "    Target_val=", target_val, "   count=", tmp.count())
 					tmp.show(false)
 					// feature_name 的点击总和
-					val emperical_df = tmp.groupBy(feature_name).agg(sum(target_val).as(feature_name+"_"+target_val+"_sum"),
-						count("user_id").as(feature_name+"_"+"user_id"+"_count"))
+					val emperical_df = tmp.groupBy(feature_name).agg(sum(target_val).as(feature_name + "_" + target_val + "_sum"),
+						count("user_id").as(feature_name + "_" + "user_id" + "_count"))
 						.orderBy(feature_name)
 					println("emperical grouped by feature_name=", feature_name)
 					emperical_df.show(false)
 					
 					val grouped_target_df = tmp.groupBy(feature_name, target_feature)
 					// 各age的点击次数总和，  求被各age点击的概率使用
-					val clicks_sum = grouped_target_df.agg(sum(target_val).as(feature_name+"_"+target_feature+"_"+target_val+"_sum"),
-						count("user_id").as(feature_name+"_"+target_feature+"_"+"user_id"+"_count"))
+					val clicks_sum = grouped_target_df.agg(sum(target_val).as(feature_name + "_" + target_feature + "_" + target_val + "_sum"),
+						count("user_id").as(feature_name + "_" + target_feature + "_" + "user_id" + "_count"))
 						.orderBy(feature_name, target_feature)
 					println("grouped by", feature_name, " and ", target_feature, " clicks sum=")
 					clicks_sum.show(false)
 					//
 					var click_likelihood = clicks_sum.join(emperical_df, usingColumn = feature_name)
-					click_likelihood = click_likelihood.withColumn(target_feature+"_click_likelihood",
-						click_likelihood(feature_name+"_"+target_feature+"_"+target_val+"_sum")/click_likelihood(feature_name+"_"+target_val+"_sum"))
+					click_likelihood = click_likelihood.withColumn(target_feature + "_click_likelihood",
+						click_likelihood(feature_name + "_" + target_feature + "_" + target_val + "_sum") / click_likelihood(feature_name + "_" + target_val + "_sum"))
 						.withColumn("user_avg_click",
-							click_likelihood(feature_name+"_"+target_feature+"_"+target_val+"_sum")/click_likelihood(feature_name+"_"+target_feature+"_"+"user_id"+"_count"))
+							click_likelihood(feature_name + "_" + target_feature + "_" + target_val + "_sum") / click_likelihood(feature_name + "_" + target_feature + "_" + "user_id" + "_count"))
 					println("click likelihood join res=")
 					click_likelihood.show(20, false)
 					
@@ -206,6 +212,5 @@ object FeatureProcess {
 		res
 	}
 	
-
 	
 }
