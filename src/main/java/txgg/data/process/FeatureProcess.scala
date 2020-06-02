d2c446c98af3debb913084c7d589bda146397193
zhongrui3 <zhongrui3@staff.weibo.com>
2020-06-02 16:00:25 +0800

    Add process

--- src/main/java/txgg/data/process/FeatureProcess.scala
+++ src/main/java/txgg/data/process/FeatureProcess.scala
@@ -128,14 +128,14 @@ object FeatureProcess {
 			   |  from (select * from txgg_temp order by time) as A group by user_id, age, gender """.stripMargin
 		val user_agg = sparkSession.sql(user_agg_sql)
 		println("user_agg info")
-		def getDuring(time_list: List[String]): Array[Float] ={
+		def getDuring(time_list: Array[String]): Array[Float] ={
 			val dur_list = time_list.map(x => x.toFloat).sorted.sliding(2).map(x => x.last - x.head).toList
 			val mean_dur = dur_list.sum/dur_list.length
 			val max_dur = dur_list.max
 			val min_dur = dur_list.min
 			Array(mean_dur, max_dur, min_dur)
 		}
-		val durUDF = udf((time_list:List[String]) => {getDuring(time_list)})
+		val durUDF = udf((time_list:Array[String]) => {getDuring(time_list)})
 		val test = user_agg.withColumn("active_avg_clicks", user_agg("all_click_cnt")*1.0/user_agg("active_days"))
 			.withColumn("dur", durUDF(col("time_list")))
 			.select(col("dur").getItem(0).as("mean_dur"),
@@ -143,10 +143,8 @@ object FeatureProcess {
 				col("dur").getItem(2).as("min_dur"))
 		println("test mean dur")
 		test.show(false)
-//		user_agg.show(false)
-		println("user feature info")
-//		user_info.show(false)
-		user_agg
+		val user_max_sql = s"""select user_id, """
+		test
 	}
 	
 	def readAllClickData(sparkSession: SparkSession, dataPath: String, savePath: String, numPartitions: Int): Dataset[Row] = {
